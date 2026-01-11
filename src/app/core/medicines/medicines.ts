b57f02f60ee2e5597bcc59ac888e525baaf40e6e
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2026-01-11 13:13:59 +0530

    New app

---
+++ src/app/core/medicines/medicines.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicines',
+  imports: [],
+  templateUrl: './medicines.html',
+  styleUrl: './medicines.css',
+})
+export class Medicines {
+
+}
