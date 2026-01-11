be506f0a0c1fc118496f4285c72e92a681d9f20e
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2026-01-11 13:31:59 +0530

    inital

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Admin } from './core/pages/admin/admin';
+import { Cart } from './core/pages/cart/cart';
+import { Home } from './core/pages/home/home';
+import { Medicines } from './core/pages/medicines/medicines';
+import { Orders } from './core/pages/orders/orders';
+import { Profile } from './core/pages/profile/profile';
+import { Store } from './core/pages/store/store';
+
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'admin', component: Admin },
+    { path: 'cart', component: Cart },
+    { path: 'medicines', component: Medicines },
+    { path: 'orders', component: Orders },
+    { path: 'profile', component: Profile },
+    { path: 'store', component: Store },
+];
